e857853e2e2b4697fa5846978ada04ed4b2b5588
perlaagomezz <gomezperla879@gmail.com>
2022-06-12 21:06:42 -0700

    added

---
+++ ticket.js
@@ -0,0 +1,51 @@
+'use strict'
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+const form = document.getElementsByTagName('*')[0];
+const lable = document.createElement('label');
+const input = document.createElement('input');
+const submit = document.createElement('input');
+const fieldset = document.getElementById('first');
+const content = document.createElement('textarea');
+
+lable.textContent = 'Your Name: '
+input.type = "text";
+input.id = "title";
+input.name = "input"
+content.id = "info"
+content.name = "content"
+content.rows = "5";
+content.cols = "30";
+submit.type = "submit";
+
+
+fieldset.appendChild(lable);
+fieldset.appendChild(input);
+fieldset.appendChild(content);
+fieldset.appendChild(submit);
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event){
+  event.preventDefault();
+  const title = event.target.title.value;
+  const content = event.target.content.value;
+
+  const h1 = document.createElement('h1');
+  const p = document.createElement('p');
+
+  h1.textContent = title;
+  p.textContent = content;
+
+  const formContent = document.getElementById('render');
+
+  formContent.appendChild(h1);
+  formContent.appendChild(p);
+}
